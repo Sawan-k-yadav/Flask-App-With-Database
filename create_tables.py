e631f6d709ebc7d4c6177673ee73ae637a04b9d2
Sawan-k-yadav <sawan.gomia@gmail.com>
2024-09-17 05:52:18 +0530

    Adding all files

---
+++ create_tables.py
@@ -0,0 +1,33 @@
+## To create tables from the database uri model
+
+# from app import app, db
+
+# with app.app_context():
+#     if not db.reflect():
+#         db.create_all()
+
+
+import pymysql
+
+
+# Creating database
+def create_database():
+    conn = pymysql.connect(
+        host='localhost',
+        user='root',
+        password='sawan',
+        port=3306
+    )
+    cursor = conn.cursor()
+    cursor.execute('CREATE DATABASE IF NOT EXISTS todo;')
+    conn.close()
+
+## for creating table
+def create_tables():
+    from app import app, db
+    with app.app_context():
+        db.create_all()
+
+if __name__ == "__main__":
+    create_database()
+    create_tables()
